211667e9f77a26bd36c8364727f3ea16f93e5e81
SJ-moon <tmdwns0710@naver.com>
2022-03-08 17:55:27 +0900

    first

---
+++ scripts/finetune_LM_on_QA_with_ST_lev3_test.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python /home/intern/seungjun/commonsense/CCL/finetune_LM_on_QA_with_ST_lev3.py --lm 'roberta-large' --pre_task 'siqa-ts9000-csqa-ts9000' --cur_task 'cmqa' --training_size 0.1 --batch_size 4 --lr 1e-6 --n_epoch 50
+CUDA_VISIBLE_DEVICES=0 python /home/intern/seungjun/commonsense/CCL/finetune_LM_on_QA_with_ST_lev3.py --lm 'roberta-large' --pre_task 'siqa-ts9000-piqa-ts9000' --cur_task 'cmqa' --training_size 0.1 --batch_size 4 --lr 1e-6 --n_epoch 50
